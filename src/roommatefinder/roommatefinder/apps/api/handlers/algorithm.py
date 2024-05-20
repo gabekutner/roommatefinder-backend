01ff9a5dcb7a0538e7dd9dc8205f11f0b696383b
gabekutner <108239517+gabekutner@users.noreply.github.com>
2024-05-20 15:50:27 -0600

    updated swipe views for algorithm

---
+++ src/roommatefinder/roommatefinder/apps/api/handlers/algorithm.py
@@ -0,0 +1,4 @@
+
+
+
+# def algorithm(user, profiles):
--- src/roommatefinder/roommatefinder/apps/api/views/swipe_views.py
+++ src/roommatefinder/roommatefinder/apps/api/views/swipe_views.py
@@ -1,9 +1,9 @@
+from django.db.models import Q
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import ListAPIView
 from rest_framework.decorators import api_view, permission_classes
-
 
 from .. import models, serializers
 from ..handlers.rank import rank_profiles
@@ -11,12 +11,33 @@ from ..pagination import StandardResultsSetPagination
 
 
 class SwipeModelViewSet(ListAPIView):
-  # queryset = models.Profile.objects.all()
+  """ get swipe profiles """
   pagination_class = StandardResultsSetPagination
   serializer_class = serializers.SwipeProfileSerializer
 
   def get_queryset(self):
-    return models.Profile.objects.all().exclude(id=self.request.user.id)
+    """ get swipe queryset - exclude self, connections & rank """
+    profiles = models.Profile.objects.all()
+    blocked_profiles = self.request.user.blocked_profiles.all()
+    connections = models.Connection.objects.filter(
+      Q(sender=self.request.user.id) | Q(receiver=self.request.user.id),
+      accepted=True
+    )
+
+    excluded_ids = connections.values_list('sender', 'receiver')
+    blocked_ids = blocked_profiles.values_list('id')
+
+    excluded_ids = set([id for sublist in excluded_ids for id in sublist])
+    blocked_ids = set([id for sublist in blocked_ids for id in sublist])
+
+    excluded_ids = excluded_ids.union(blocked_ids)
+    
+    profiles = profiles.exclude(
+      id__in=excluded_ids
+    )
+  
+    return profiles
+
 
 @api_view(["get"])
 @permission_classes([IsAuthenticated])
