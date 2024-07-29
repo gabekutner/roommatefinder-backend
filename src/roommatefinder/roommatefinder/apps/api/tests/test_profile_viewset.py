b48099c2694dc497334dc25fcc52b2638e9e8dd1
gabekutner <108239517+gabekutner@users.noreply.github.com>
2024-07-29 15:48:48 -0700

    test

--- src/roommatefinder/roommatefinder/apps/api/tests/test_profile_viewset.py
+++ src/roommatefinder/roommatefinder/apps/api/tests/test_profile_viewset.py
@@ -12,13 +12,12 @@ class TestProfileModelViewSet(unittest.TestCase):
     # self.unauthed_user = models.Profile.objects.get(identifier="4153213")
 
   def test_list_unauthed(self):
-    profile = models.Profile.objects.all()
-    print(profile)
-  #   view = views.profile_views.ProfileViewSet.as_view({'get': 'list'})
-  #   request = self.factory.get("/")
-  #   force_authenticate(request, user=self.unauthed_user)
-  #   response = view(request)
-  #   assert response.status_code == 401
+    profile = models.Profile.objects.create(identifier="dave")
+    view = views.profile_views.ProfileViewSet.as_view({'get': 'list'})
+    request = self.factory.get("/")
+    force_authenticate(request, user=profile)
+    response = view(request)
+    assert response.status_code == 401
 
   # def test_list_authed(self):
   #   view = views.profile_views.ProfileViewSet.as_view({'get': 'list'})
