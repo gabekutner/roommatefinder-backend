e6ec02bd66c0157630c7ec29c742de9b24dc3747
gabekutner <108239517+gabekutner@users.noreply.github.com>
2024-07-29 14:12:05 -0700

    try again

---
+++ .github/run_tests_on_api.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+python3 src/roommatefinder/manage.py test roommatefinder.apps.api --settings=roommatefinder.settings.test
--- src/roommatefinder/run_tests_on_api.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-python3 manage.py test roommatefinder.apps.api --settings=roommatefinder.settings.test
