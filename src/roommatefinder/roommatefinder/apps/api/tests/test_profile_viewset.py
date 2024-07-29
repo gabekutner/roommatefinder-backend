ec844f50e31208d3c2a44ecd68a30aa85eaacee1
gabekutner <108239517+gabekutner@users.noreply.github.com>
2024-07-29 16:23:00 -0700

    raa!!! tests working!!!

--- src/roommatefinder/roommatefinder/apps/api/tests/test_profile_viewset.py
+++ src/roommatefinder/roommatefinder/apps/api/tests/test_profile_viewset.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from rest_framework.test import APIRequestFactory
+from rest_framework.test import APIRequestFactory, force_authenticate
 from .. import models, views
 
 class TestProfileModelViewSet(TestCase):
@@ -8,10 +8,21 @@ class TestProfileModelViewSet(TestCase):
     def setUp(self):
         """Setup for the tests"""
         self.factory = APIRequestFactory()
-        self.profile = models.Profile.objects.create(identifier="gabe")
+        self.unauthed_user = models.Profile.objects.create(identifier="dave", is_superuser=False, otp_verified=True)
+        self.authed_user = models.Profile.objects.create(identifier="gabe", is_superuser=True, otp_verified=True)
 
     def test_list_unauthed(self):
-        """Test listing profiles without authentication"""
-        response = views.profile_views.ProfileViewSet.as_view({'get': 'list'})
-        self.assertEqual(response.status_code, 200)
-        # Add more assertions as needed
+      """ Test listing profiles without authentication """
+      request = self.factory.get("/")
+      view = views.profile_views.ProfileViewSet.as_view({'get': 'list'})
+      force_authenticate(request, user=self.unauthed_user)
+      response = view(request)
+      self.assertEqual(response.status_code, 401)
+    
+    def test_list_authed(self):
+      """ Test listing profiles without authentication """
+      request = self.factory.get("/")
+      view = views.profile_views.ProfileViewSet.as_view({'get': 'list'})
+      force_authenticate(request, user=self.authed_user)
+      response = view(request)
+      self.assertEqual(response.status_code, 200)
