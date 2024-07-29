66fee0c38a0e0902725b4889d7d334b3a388813a
gabekutner <108239517+gabekutner@users.noreply.github.com>
2024-07-29 15:34:56 -0700

    try again

--- src/roommatefinder/roommatefinder/settings/test.py
+++ src/roommatefinder/roommatefinder/settings/test.py
@@ -1,1 +1,14 @@
+import os
 from ._base import *
+
+if os.environ.get('GITHUB_WORKFLOW'):
+  DATABASES = {
+    'default': {
+      'ENGINE': 'django.db.backends.postgresql',
+      'NAME': 'github_actions',
+      'USER': 'postgres',
+      'PASSWORD': 'postgres',
+      'HOST': '127.0.0.1',
+      'PORT': '5432',
+    }
+  }
