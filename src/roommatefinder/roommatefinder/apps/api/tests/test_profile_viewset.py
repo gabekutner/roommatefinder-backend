cc9f967138b82a1e8dd5154f2897fe2810e294cf
gabekutner <108239517+gabekutner@users.noreply.github.com>
2024-07-29 16:12:08 -0700

    again

--- src/roommatefinder/roommatefinder/apps/api/tests/test_profile_viewset.py
+++ src/roommatefinder/roommatefinder/apps/api/tests/test_profile_viewset.py
@@ -1,28 +1,17 @@
-import unittest
-from rest_framework.test import APIRequestFactory, force_authenticate
-from .. import models, views
+from django.test import TestCase
+from rest_framework.test import APIRequestFactory
+from . import models, views
 
+class TestProfileModelViewSet(TestCase):
+    """Test ProfileModelViewSet"""
 
-class TestProfileModelViewSet(unittest.TestCase):
-  """ Test ProfileModelViewSet """
-  def __init__(self, *args, **kwargs):
-    super(TestProfileModelViewSet, self).__init__(*args, **kwargs)
-    self.factory = APIRequestFactory()
-    # self.authed_user = models.Profile.objects.get(identifier="gabekutner")
-    # self.unauthed_user = models.Profile.objects.get(identifier="4153213")
+    def setUp(self):
+        """Setup for the tests"""
+        self.factory = APIRequestFactory()
+        self.profile = models.Profile.objects.create(identifier="gabe")
 
-  def test_list_unauthed(self):
-    profile = models.Profile(identifier="gabe")
-    profile.save()
-    # view = views.profile_views.ProfileViewSet.as_view({'get': 'list'})
-    # request = self.factory.get("/")
-    # force_authenticate(request, user=profile)
-    # response = view(request)
-    # assert response.status_code == 401
-
-  # def test_list_authed(self):
-  #   view = views.profile_views.ProfileViewSet.as_view({'get': 'list'})
-  #   request = self.factory.get("/")
-  #   force_authenticate(request, user=self.authed_user)
-  #   response = view(request)
-  #   assert response.status_code == 200
+    def test_list_unauthed(self):
+        """Test listing profiles without authentication"""
+        response = views.ProfileViewSet.as_view({'get': 'list'})
+        self.assertEqual(response.status_code, 200)
+        # Add more assertions as needed
