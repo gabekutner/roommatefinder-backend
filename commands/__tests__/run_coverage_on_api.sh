12f1881fa3f2c9d606182185b3c846b7aa53c911
gabekutner <108239517+gabekutner@users.noreply.github.com>
2024-07-29 14:46:12 -0700

    update the coverage test

--- commands/__tests__/run_coverage_on_api.sh
+++ commands/__tests__/run_coverage_on_api.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
+
+echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
+
+MIN_COVERAGE=25
+
 coverage erase
 coverage run --source=roommatefinder.apps.api src/roommatefinder/manage.py test roommatefinder.apps.api --settings=roommatefinder.settings.test
-coverage report
+
+# get coverage report
+COVERAGE_RESULT=`coverage report | grep TOTAL | awk 'N=1 {print $NF}' | sed 's/%//g'`
+if [[ $COVERAGE_RESULT -gt $MIN_COVERAGE ]]; then
+  echo ::set-output name=coverage_result::$COVERAGE_RESULT
+else
+  echo "Code coverage below allowed threshold ($COVERAGE_RESULT<$MIN_COVERAGE)"
+  exit 1
+fi 
+
+echo "Completed ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
