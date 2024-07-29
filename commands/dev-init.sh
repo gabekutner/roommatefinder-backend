48edaa339644c781cc76536cc54dda68b2d383c8
gabekutner <108239517+gabekutner@users.noreply.github.com>
2024-07-28 21:29:59 -0700

    dev init sh good

--- commands/dev-init.sh
+++ commands/dev-init.sh
@@ -7,12 +7,13 @@
 #
 # This script requires the jq runtime dependency : https://jqlang.github.io/jq/download/
 
-# Usage: $ bash commands/dev-init.sh "<database>" "<email>" "<email-passphrase>"
+# Usage: $ bash commands/dev-init.sh "<email>" "<passphrase-1>" "<passphrase-2>" "<passphrase-3>" "<passphrase-4>" "<database>"
 ## NOTE: For development, I use the same string - "postgres" - for DATABASE_NAME, DATABASE_USER AND DATABASE_PASSWORD
 
 # Check parameters
-if [ "$#" -ne 3 ]; then
-    echo "$ bash commands/dev-init.sh <database> <email> <email-passphrase>"
+if [ "$#" -ne 6 ]; then
+    echo "$ bash commands/dev-init.sh <email> <passphrase-1> <passphrase-2> <passphrase-3> <passphrase-4> <database>"
+    echo "## Passphrases for email app passwords are 4 letters long. Enter them after the associated email. "
     exit 1
 fi
 
@@ -37,20 +38,26 @@ python3 -m venv env
 # Activate the virtual environment
 source env/bin/activate
 
+# Log
+echo "🥽 Created venv"
+
 # Download development requirements
 python3 -m pip install -r "${PROJECTPATH}/requirements/_base.txt"
 
+# Log
+echo "🐍 Installed project dependencies"
+
 # Write the sample_secrets.json file
 jq -n --arg secret_key "$SECRETKEY" \
-      --arg database_name $1 \
-      --arg database_user $1 \
-      --arg database_password $1 \
+      --arg database_name $6 \
+      --arg database_user $6 \
+      --arg database_password $6 \
       --arg database_host "localhost" \
       --argjson database_port 5432 \
       --arg email_host "smtp.gmail.com" \
       --arg email_port 587 \
-      --arg email_host_user $2 \
-      --arg email_host_password $3 \
+      --arg email_host_user $1 \
+      --arg email_host_password "$2 $3 $4 $5" \
 '{
   SECRET_KEY: $secret_key,
   DATABASE_NAME: $database_name,
@@ -63,3 +70,6 @@ jq -n --arg secret_key "$SECRETKEY" \
   EMAIL_HOST_USER: $email_host_user,
   EMAIL_HOST_PASSWORD: $email_host_password
 }' > "${PROJECTPATH}/roommatefinder/settings/sample_secrets.json"
+
+# Log
+echo "🔑 Created secrets file"
