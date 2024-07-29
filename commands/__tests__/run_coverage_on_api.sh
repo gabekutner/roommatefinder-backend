bd7aa5adb667a0cc895b04ce06eff7e706d4c542
gabekutner <108239517+gabekutner@users.noreply.github.com>
2024-07-29 14:31:05 -0700

    run coverage action

---
+++ commands/__tests__/run_coverage_on_api.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+coverage erase
+coverage run --source=src.roommatefinder.roommatefinder.apps.api manage.py test src.roommatefinder.roommatefinder.apps.api --settings=src.roommatefinder.roommatefinder.settings.test
+coverage report
