0e26b9fd6e1d1fd4e18cf42f0d952a51445ba2f8
gabekutner <108239517+gabekutner@users.noreply.github.com>
2024-07-30 17:00:21 -0700

    omit routing.py in coverage report

--- commands/__tests__/manual_coverage.sh
+++ commands/__tests__/manual_coverage.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 coverage erase
-coverage run --source=roommatefinder.apps.api src/roommatefinder/manage.py test roommatefinder.apps.api --settings=roommatefinder.settings.test
+coverage run --omit="*/routing.py" --source=roommatefinder.apps.api src/roommatefinder/manage.py test roommatefinder.apps.api --settings=roommatefinder.settings.test
 coverage report
